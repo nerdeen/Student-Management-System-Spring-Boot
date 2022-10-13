2719fd403917446e4e7e0d1398566a08a7d3ab71
nerdeen <nerdeen.ahmad15@gmail.com>
2022-10-13 16:04:51 +0200

    app with controller service changes

--- src/main/java/com/example/democlassapp/facade/AssignmentFacade.java
+++ src/main/java/com/example/democlassapp/facade/AssignmentFacade.java
@@ -1,4 +1,14 @@
 package com.example.democlassapp.facade;
 
 public interface AssignmentFacade {
+
+    public void add(AssignmentDTO assignmentDTO);
+
+    public void update(AssignmentDTO assignmentDTO);
+
+    public void deleteById(int id);
+
+    public List<AssignmentDTO> findAll();
+
+    public AssignmentDTO findById(int id);
 }
